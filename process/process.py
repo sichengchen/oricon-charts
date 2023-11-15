622d1c477eb8298fb49192811c51c4ae344f461d
sichengchen <csc0330@hotmail.com>
2023-11-15 13:30:51 -0500

    Update page titles and headings

--- process/process.py
+++ process/process.py
@@ -48,12 +48,12 @@ html_content = f"""
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Charts - {year}</title>
+        <title>Oricon Charts - {year}</title>
         <link rel="stylesheet" href="../style.css">
     </head>
     <body>
         <header>
-        <h1><a href="../index.html">Oricon Weekly Charts</a> - {year}</h1>
+        <h1><a href="../index.html">Oricon Charts</a> - {year}</h1>
 """
 
 # Initialize the navigation element
